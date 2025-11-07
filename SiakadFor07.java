997d0c63855a56218d0895206828afa396f6ba83
auliazahwaa007 <auliazahwaa007@gmail.com>
2025-11-07 10:00:28 +0700

    program setelah modifikasi

--- SiakadFor07.java
+++ SiakadFor07.java
@@ -4,9 +4,11 @@ public class SiakadFor07 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        double nilai;
+        double nilai = 0;
         double tertinggi = 0;
         double terendah = 100;
+        int lulus = 0;
+        int tidakLulus = 0;
         
         for (int i = 1; i <= 10; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + i + "; ");
@@ -17,9 +19,16 @@ public class SiakadFor07 {
             if (nilai < terendah) {
                 terendah = nilai;
             }
+            if (nilai >= 60) {
+                lulus++;
+            } else {
+                tidakLulus++;
+            }
         }
         System.out.println("Nilai tertinggi; " + tertinggi);
         System.out.println("Nilai Terendah; " + terendah);
+        System.out.println("Banyaknya mahasiswa lulus: " + lulus);
+        System.out.println("Banyaknya mahasiswa tidak lulus: " + tidakLulus);
 
         sc.close();
     }
